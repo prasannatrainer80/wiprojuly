d20968defee4c7532900d699dccc6968a682b1b0
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-08-02 10:14:34 +0530

    code

--- Aug_1_Part_1/Friday_Query_Working.sql
+++ Aug_1_Part_1/Friday_Query_Working.sql
@@ -24,4 +24,13 @@
 	6-datepart(dw,dateadd(dd,1,dateadd(mm,-1,DATEADD(SS,-1,DATEADD(mm, DATEDIFF(mm,0,DATEADD(MM,1,GETDATE())),0)))))
 	,
 	dateadd(dd,1,dateadd(mm,-1,DATEADD(SS,-1,DATEADD(mm, DATEDIFF(mm,0,DATEADD(MM,1,GETDATE())),0)))))))) 
-	'Fourth Friday'
+	'Fourth Friday', 
+	DateAdd(dd,7, 
+	DATEADD(dd,7,
+	DateAdd(dd,7, 
+	DATEADD(DD,7,
+	dateadd(dd,
+	6-datepart(dw,dateadd(dd,1,dateadd(mm,-1,DATEADD(SS,-1,DATEADD(mm, DATEDIFF(mm,0,DATEADD(MM,1,GETDATE())),0)))))
+	,
+	dateadd(dd,1,dateadd(mm,-1,DATEADD(SS,-1,DATEADD(mm, DATEDIFF(mm,0,DATEADD(MM,1,GETDATE())),0))))))))) 
+	'Fifth Friday'
