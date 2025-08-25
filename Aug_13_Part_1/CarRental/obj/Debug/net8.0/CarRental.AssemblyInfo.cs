dd2b4780e42c0e76e25a774ea8be729fcfffd712
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-08-25 16:09:23 +0530

    code

--- Aug_13_Part_1/CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
+++ Aug_13_Part_1/CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bede2f2c1e5aafb77f76b0422d2ca322c8fcdc5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3ed18907036207abb7128cbeec294da52afd8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
